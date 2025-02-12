6890ea6fdcee24dd3c9e037eb4ffa58929977bff
AmirCNY <amirhosseintorkamansohrabi@gmail.com>
2025-02-12 14:40:42 +0100

    this is new version of search

--- oopEX3.py
+++ oopEX3.py
@@ -26,7 +26,7 @@ class shops:
     def Dell_book(self,name):
       self.products.pop(name)
       print ("Dell")
-
+#.......................................................................
     def Search(self,name):
       if self.products != None:
         name_search=self.products.get(name,"Name not found")
@@ -42,7 +42,29 @@ class shops:
         
       else:
         print("It is Empaty")
+#...................................profational Search................
+    def Profation_Search(self):
+       if self.products:
+        for k in self.products:
+            print(f"{k}") 
+        sh=input("Would you like Edit?please name")
+        if(sh in self.products):
+          print(self.products[sh])   
+          data=input("witch one you would like to change")
+          if(data=="price"):
+             new_price=float(input("please inset new price"))
+             self.products[sh].price=new_price
+          elif(data=="Year"):
+             new_year=int(input("please inset new Year"))
+             self.products[sh].year=new_year
+          elif(data=="publisher"):
+             new_publisher=input("please inset new publisher")
+             self.products[sh].publisher= new_publisher
+          else:
+            print("I canot find")
 
+       else:
+        print("we donot have Products")    
     
     def __str__(self):
      return f"name:{self.name},products:{self.products}\n,Number of books:{len(self.products)}" 
@@ -63,6 +85,7 @@ sh.add_book(b5)
 sh.add_book(b6)
 sh.Search(b6.name)
 print(sh)
+sh.Profation_Search()
 #sh.Dell_book(b1.name)
 #...sh.Dell_book("b1")
 #
